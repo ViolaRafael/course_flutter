d43b383b06bb77141b27d98483072cc17eb06b75
ViolaRafael <rafaelviola.eprosec@gmail.com>
2022-11-29 15:33:03 -0100

    Fix Resposta

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ class PerguntaAppState extends State<PerguntaApp> {
     setState(() {
       _perguntaSelecionada++;
     });
-    print('Pergunta respondida!');
   }
   @override
   Widget build(BuildContext context){
@@ -27,9 +26,9 @@ class PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: [
             Questao(perguntas[_perguntaSelecionada]),
-            Resposta('Resposta 1'),
-            Resposta('Resposta 2'),
-            Resposta('Resposta 3'),
+            Resposta('Resposta 1', _responder),
+            Resposta('Resposta 2', _responder),
+            Resposta('Resposta 3', _responder),
           ],
         ),
       ),
--- lib/resposta.dart
+++ lib/resposta.dart
@@ -2,14 +2,23 @@ import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
   final String texto;
+  final void Function() onSelecao;
 
-  Resposta(this.texto);
+  Resposta(this.texto, this.onSelecao);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      child: Text(texto),
-      onPressed: null,
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        child: Text(texto),
+        onPressed: onSelecao,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue,
+          padding: EdgeInsets.all(10),
+          textStyle: TextStyle(fontSize: 18)
+        ),
+      ),
     );
   }
 }
