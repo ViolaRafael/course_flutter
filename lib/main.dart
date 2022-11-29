460a30af0ccf8ad430b2edbf11bd6592f259c518
ViolaRafael <rafaelviola.eprosec@gmail.com>
2022-11-29 15:57:32 -0100

    Fix List Feature

--- lib/main.dart
+++ lib/main.dart
@@ -6,29 +6,42 @@ main() => runApp(new PerguntaApp());
 
 class PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
-  void _responder(){
+  void _responder() {
     setState(() {
       _perguntaSelecionada++;
     });
   }
+
   @override
-  Widget build(BuildContext context){
-    final List<String> perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?',
+  Widget build(BuildContext context) {
+    final List<Map<String, Object>> perguntas = [
+      {
+        'texto': 'Qual é a sua cor favorita?',
+        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+      },
+      {
+        'texto': 'Qual é o seu animal favorito?',
+        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+      },
+      {
+        'texto': 'Qual é o seu instrutor favorito?',
+        'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+      }
     ];
-    
+    List<Widget> respostas = [];
+    for(String textoResp in perguntas[_perguntaSelecionada].cast()['respostas']){
+      respostas.add(Resposta(textoResp, _responder));
+    }
+
     return MaterialApp(
-      home: Scaffold (
+      home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
         body: Column(
           children: [
-            Questao(perguntas[_perguntaSelecionada]),
-            Resposta('Resposta 1', _responder),
-            Resposta('Resposta 2', _responder),
-            Resposta('Resposta 3', _responder),
+            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
+            ...respostas
           ],
         ),
       ),
